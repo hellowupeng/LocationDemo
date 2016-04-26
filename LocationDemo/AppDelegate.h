6225fc36377bf23cbf8f06d05e7b87279dc4a4d8
吴鹏 <457760698@qq.com>
2016-04-26 15:37:56 +0800

    Initial Commit

---
+++ LocationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationDemo
+//
+//  Created by gaiaworks on 16/4/26.
+//  Copyright © 2016年 gaiaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
